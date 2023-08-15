5266b1e95a9757bbab6353b87dd8022010c64a81
philippbenkert <76922498+philippbenkert@users.noreply.github.com>
2023-08-15 07:27:28 +0200

    Update ESP32S3.ino

--- ESP32S3.ino
+++ ESP32S3.ino
@@ -10,74 +10,36 @@
 
 WebServer server(80);
 
-const int SENSOR_PIN = 10;
-const int MQTT_PORT = 1024;
-const int OTA_PORT = 80;
-const int SERIAL_BAUD_RATE = 115200;
-const int GPS_BAUD_RATE = 9600;
-const int RECONNECT_DELAY = 5000;
+// Constants
+const int SENSOR_PIN = 10, MQTT_PORT = 1024, OTA_PORT = 80, SERIAL_BAUD_RATE = 115200, GPS_BAUD_RATE = 9600;
+const int SER_Pin = 7, RCLK_Pin = 5, SRCLK_Pin = 6, numOfShiftRegisters = 1, ssrPin = 12;
+const int PWM_FREQUENCY = 1000, PWM_RESOLUTION = 8;
+const float SHUNT_RESISTOR = 150.0, MIN_MA = 4.0, MAX_MA = 20.0, MIN_PRESSURE = 0.0, MAX_PRESSURE = 100.0;
+const double outlierThreshold = 10.0;
 
-void callback(char* topic, byte* payload, unsigned int length);
-void sendData();
-boolean reconnect();
-void berechneUndSendeDruck();
-void setup_wifi();
-void handleGPSData();
+// Variables
+double currentLatitude = 0.0, currentLongitude = 0.0, kalmanGain = 0.0, previousLatitude = 0.0, previousLongitude = 0.0;
+float pressure = 0.0, MinDruck = 0.0, TankLimitDruck = 0.05;
+bool Wasserversorgung = false, Boilerheizung = false, UVCLicht = false, isOutlier = false, isParked = false;
+unsigned long lastSendTime = 0;
 
-
-// Kalman Filter variables
-double currentLatitude = 0.0;
-double currentLongitude = 0.0;
-double kalmanGain = 0.0;
-double previousLatitude = 0.0;
-double previousLongitude = 0.0;
-
-const float SHUNT_RESISTOR = 150.0; // in Ohm
-const float MIN_MA = 4.0;  // Minimum 4mA
-const float MAX_MA = 20.0; // Maximum 20mA
-const float MIN_PRESSURE = 0.0; // Minimum Druckwert (angepasst an Ihren Sensor)
-const float MAX_PRESSURE = 100.0; // Maximum Druckwert (angepasst an Ihren Sensor)
-
-// Outlier detection variables
-const double outlierThreshold = 10.0;  // Adjust the threshold as needed
-bool isOutlier = false;
-
-// Replace with your network credentials
+// Network credentials
 const char* ssid = "SSID";
 const char* password = "PASSWORD";
-
-unsigned long lastSendTime = 0;
-bool isParked = false;
-
-// Replace with your MQTT Broker address
 const char* mqtt_server = "192.168.0.1";
 
-// Shift register pins
-const int SER_Pin = 7;    //pin 14 on the 75HC595
-const int RCLK_Pin = 5;   //pin 12 on the 75HC595
-const int SRCLK_Pin = 6;  //pin 11 on the 75HC595
-
-// Number of shift registers
-const int numOfShiftRegisters = 1;
-
-// Additional SSR pin
-const int ssrPin = 12;
-const int PWM_FREQUENCY = 1000; // PWM-Frequenz in Hz
-const int PWM_RESOLUTION = 8; // Auflösung der PWM (8 Bit = 0-255)
-
-
-// The serial connection to the GPS device
-SoftwareSerial ss(11, 13);  // RX, TX
-
+SoftwareSerial ss(11, 13);
 TinyGPSPlus gps;
-
-// Create a ShiftRegister object
 ShiftRegister74HC595<numOfShiftRegisters> sr(SER_Pin, RCLK_Pin, SRCLK_Pin);
-
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup_wifi();
+void callback(char* topic, byte* payload, unsigned int length);
+void sendData();
+boolean reconnect();
+void berechneUndSendeDruck();
+void handleGPSData();
 
 void setup() {
     Serial.begin(SERIAL_BAUD_RATE);
@@ -86,29 +48,46 @@ void setup() {
     client.setServer(mqtt_server, MQTT_PORT);
     client.setCallback(callback);
     pinMode(ssrPin, OUTPUT);
-    ledcSetup(0, PWM_FREQUENCY, PWM_RESOLUTION); // Kanal 0, Frequenz, Auflösung
-    ledcAttachPin(ssrPin, 0); // Pin zu Kanal 0 zuweisen
+    ledcSetup(0, PWM_FREQUENCY, PWM_RESOLUTION);
+    ledcAttachPin(ssrPin, 0);
 
-    // OTA setup
     MDNS.begin("esp32");
     server.begin();
     MDNS.addService("http", "tcp", OTA_PORT);
-    Serial.printf("HTTPUpdateServer bereit! Öffnen Sie http://%s.local/update in Ihrem Browser\n", WiFi.getHostname());
+    Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", WiFi.getHostname());
     
-    // Zustände der Relais beim Booten senden
-    client.publish("/relay1", "off");
-    client.publish("/relay2", "off");
-    client.publish("/relay3", "off");
-    client.publish("/relay4", "off");
-    client.publish("/relay5", "off");
-    client.publish("/relay6", "off");
-    client.publish("/ssrPower", "0"); // Setzt die Heizleistung auf 0% beim Booten
+    for (int i = 0; i < 6; i++) {
+        char relayTopic[10];
+        snprintf(relayTopic, sizeof(relayTopic), "/relay/%d", i + 1);
+        client.publish(relayTopic, "off");
+        client.subscribe(relayTopic);
+    }
+    client.publish("/ssr", "off");
+    client.subscribe("/ssr");
+    client.publish("/ssrPower", "0");
+    client.publish("/TankLimitDruck", String(TankLimitDruck).c_str());
+    client.publish("/Wasserversorgung", Wasserversorgung ? "on" : "off");
+    client.publish("/Boilerheizung", Boilerheizung ? "on" : "off");
+    client.publish("/UVCLicht", UVCLicht ? "on" : "off");
 }
-
 
 void loop() {
     handleGPSData();
     berechneUndSendeDruck();
+    if (pressure <= TankLimitDruck || !Wasserversorgung) {
+        Boilerheizung = false;
+        Wasserversorgung = false;
+        sr.set(5, LOW);
+        setHeatingPower(0);
+    }
+    sr.set(5, Wasserversorgung ? HIGH : LOW);
+    if (Boilerheizung) {
+        sr.set(5, HIGH);
+        delay(10000);
+        setHeatingPower(100);
+    } else {
+        setHeatingPower(0);
+    }
     ArduinoOTA.handle();
     if (!client.connected()) {
         reconnect();
@@ -121,30 +100,24 @@ void setHeatingPower(int percentage) {
     ledcWrite(0, pwmValue);
 }
 
-
 void handleGPSData() {
     while (ss.available() > 0) {
         gps.encode(ss.read());
         if (gps.location.isUpdated()) {
-            handleFahrzeugStatus();
+            if (gps.speed.kmph() < 2.0) {
+                if (!isParked) {
+                    lastSendTime = millis();
+                    isParked = true;
+                } else if (millis() - lastSendTime >= 1800000) {
+                    sendData();
+                    lastSendTime = millis();
+                }
+            } else if (millis() - lastSendTime >= 1000 || isParked) {
+                sendData();
+                lastSendTime = millis();
+                isParked = false;
+            }
         }
-    }
-}
-
-void handleFahrzeugStatus() {
-    // Überprüfen Sie, ob das Fahrzeug geparkt ist
-    if (gps.speed.kmph() < 2.0) {
-        if (!isParked) {
-            lastSendTime = millis();
-            isParked = true;
-        } else if (millis() - lastSendTime >= 1800000) {
-            sendData();
-            lastSendTime = millis();
-        }
-    } else if (millis() - lastSendTime >= 1000 || isParked) {
-        sendData();
-        lastSendTime = millis();
-        isParked = false;
     }
 }
 
@@ -152,10 +125,9 @@ void berechneUndSendeDruck() {
     int sensorValue = analogRead(SENSOR_PIN);
     float voltage = sensorValue * (3.3 / 4095.0);
     float current = voltage / SHUNT_RESISTOR;
-    float pressure = MIN_PRESSURE + ((current - MIN_MA) / (MAX_MA - MIN_MA)) * (MAX_PRESSURE - MIN_PRESSURE);
-
+    pressure = MIN_PRESSURE + ((current - MIN_MA) / (MAX_MA - MIN_MA)) * (MAX_PRESSURE - MIN_PRESSURE);
     static float lastPressure = 0.0;
-    if (isParked && abs(pressure - lastPressure) > 1.0) {  // Änderung von 1.0 als Beispiel für eine "wesentliche" Änderung
+    if (isParked && abs(pressure - lastPressure) > 1.0) {
         Serial.println(pressure);
         char pressureMsg[50];
         snprintf(pressureMsg, 50, "%f", pressure);
@@ -165,49 +137,29 @@ void berechneUndSendeDruck() {
 }
 
 void sendData() {
-  // Apply Kalman Filter
-  kalmanGain = 0.4;  // Adjust the Kalman gain as needed
-  double filteredLatitude = previousLatitude + kalmanGain * (gps.location.lat() - previousLatitude);
-  double filteredLongitude = previousLongitude + kalmanGain * (gps.location.lng() - previousLongitude);
-
-  // Check for outliers
-  isOutlier = false;
-  double latitudeDifference = abs(filteredLatitude - gps.location.lat());
-  double longitudeDifference = abs(filteredLongitude - gps.location.lng());
-  if (latitudeDifference > outlierThreshold || longitudeDifference > outlierThreshold) {
-    isOutlier = true;
-  }
-
-  // Update previous values for the next iteration
-  previousLatitude = filteredLatitude;
-  previousLongitude = filteredLongitude;
-
-  // Send data only if it is not an outlier
-  if (!isOutlier) {
-    char latMsg[50];
-    snprintf(latMsg, 50, "%f", filteredLatitude);
-    client.publish("latitude", latMsg);
-
-    char lonMsg[50];
-    snprintf(lonMsg, 50, "%f", filteredLongitude);
-    client.publish("longitude", lonMsg);
-  }
-
-  // Send Speed
-  char speedMsg[50];
-  snprintf(speedMsg, 50, "%f", gps.speed.kmph());
-  client.publish("speed", speedMsg);
-
-  // Send Altitude
-  char altMsg[50];
-  snprintf(altMsg, 50, "%f", gps.altitude.meters());
-  client.publish("altitude", altMsg);
+    kalmanGain = 0.4;
+    double filteredLatitude = previousLatitude + kalmanGain * (gps.location.lat() - previousLatitude);
+    double filteredLongitude = previousLongitude + kalmanGain * (gps.location.lng() - previousLongitude);
+    isOutlier = abs(filteredLatitude - gps.location.lat()) > outlierThreshold || abs(filteredLongitude - gps.location.lng()) > outlierThreshold;
+    previousLatitude = filteredLatitude;
+    previousLongitude = filteredLongitude;
+    if (!isOutlier) {
+        char latMsg[50], lonMsg[50], speedMsg[50], altMsg[50];
+        snprintf(latMsg, 50, "%f", filteredLatitude);
+        snprintf(lonMsg, 50, "%f", filteredLongitude);
+        snprintf(speedMsg, 50, "%f", gps.speed.kmph());
+        snprintf(altMsg, 50, "%f", gps.altitude.meters());
+        client.publish("latitude", latMsg);
+        client.publish("longitude", lonMsg);
+        client.publish("speed", speedMsg);
+        client.publish("altitude", altMsg);
+    }
 }
 
 void setup_wifi() {
     delay(10);
     Serial.println();
-    Serial.print("Verbindung zu ");
+    Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -215,81 +167,61 @@ void setup_wifi() {
         Serial.print(".");
     }
     Serial.println("");
-    Serial.println("WiFi verbunden");
-    Serial.println("IP-Adresse: ");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 }
 
+void handleRelays(int index, const char* payload) {
+    sr.set(index, strcmp(payload, "on") == 0 ? HIGH : LOW);
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
-  String messageTemp;
-  for (int i = 0; i < length; i++) {
-    messageTemp += (char)payload[i];
-  }
-
-  // If a message is received on the topic, you check if the message is either "on" or "off".
-  for (int i = 0; i < 6; i++) {
-    String relayTopic = "/relay/" + String(i + 1);
-    if (String(topic) == relayTopic) {
-      if (messageTemp == "on") {
-        sr.set(i, HIGH);
-      } else if (messageTemp == "off") {
-        sr.set(i, LOW);
-      }
+    payload[length] = '\0';
+    if (strncmp(topic, "/relay/", 7) == 0) {
+        int relayIndex = topic[7] - '1';
+        if (relayIndex >= 0 && relayIndex < 6) {
+            handleRelays(relayIndex, (char*)payload);
+            return;
+        }
     }
-  }
-
-  // For SSR
-  if (String(topic) == "/ssrPower") {
-        String messageTemp;
-        for (int i = 0; i < length; i++) {
-            messageTemp += (char)payload[i];
-        }
-        int powerPercentage = messageTemp.toInt();
-        setHeatingPower(powerPercentage);
-    } else if (String(topic) == "/ssr") {
-        // Behalten Sie den vorhandenen Code bei, um das SSR ein- und auszuschalten
-        String messageTemp;
-        for (int i = 0; i < length; i++) {
-            messageTemp += (char)payload[i];
-        }
-        if (messageTemp == "on") {
-            setHeatingPower(100); // Setzt die Heizleistung auf 100%
-        } else if (messageTemp == "off") {
-            setHeatingPower(0); // Schaltet die Heizung aus
-        }
+    const char* topicStr = (char*)topic;
+    if (strcmp(topicStr, "/ssrPower") == 0) {
+        setHeatingPower(atoi((char*)payload));
+    } else if (strcmp(topicStr, "/ssr") == 0) {
+        setHeatingPower(strcmp((char*)payload, "on") == 0 ? 100 : 0);
+    } else if (strcmp(topicStr, "/Wasserversorgung") == 0) {
+        Wasserversorgung = strcmp((char*)payload, "on") == 0;
+    } else if (strcmp(topicStr, "/Boilerheizung") == 0) {
+        Boilerheizung = strcmp((char*)payload, "on") == 0;
+    } else if (strcmp(topicStr, "/UVCLicht") == 0) {
+        UVCLicht = strcmp((char*)payload, "on") == 0;
+    } else if (strcmp(topicStr, "/MinDruck") == 0) {
+        MinDruck = atof((char*)payload);
     }
 }
 
 boolean reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP32Client")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      for (int i = 0; i < 6; i++) {
-        String relayTopic = "/relay/" + String(i + 1);
-        client.publish(relayTopic.c_str(), "off");
-        client.subscribe(relayTopic.c_str());
-      }
-
-      // For SSR
-      client.publish("/ssr", "off");
-      client.subscribe("/ssr");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+    while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect("ESP32Client")) {
+            Serial.println("connected");
+            for (int i = 0; i < 6; i++) {
+                char relayTopic[10];
+                snprintf(relayTopic, sizeof(relayTopic), "/relay/%d", i + 1);
+                client.publish(relayTopic, "off");
+                client.subscribe(relayTopic);
+            }
+            client.publish("/ssr", "off");
+            client.subscribe("/ssr");
+            return true;
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
     }
-  }
-
-  // If MQTT connection is lost, turn off SSR
-  if (!client.connected()) {
     digitalWrite(ssrPin, LOW);
-  }
-
-  return client.connected();
+    return false;
 }
